321acd8797d2287a9799ae4ca8eb7b80da60ff55
Paulo <paulopires-20022002@hotmail.com>
2018-09-22 23:11:30 +0100

    botavalon

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
 client.on("ready", () => {
   console.log(`Estou on na AvalonNetwork com ${client.users.size} players!`); 
   client.user.setActivity(`@Network_Avalon`);
